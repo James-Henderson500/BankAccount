5a8e93fd1feb8b480c745074335e77ffaa85f92e
James-Henderson500 <james.henderson4@homeoffice.gov.uk>
2025-11-12 10:08:58 +0000

    Further interest

--- obj/Debug/net9.0/BankAccount.AssemblyInfo.cs
+++ obj/Debug/net9.0/BankAccount.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankAccount")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec554875b0c6e114d60be27928df056c069ec3b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e9c17ffde0067d73018484bb2b63f6830c2d83")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankAccount")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankAccount")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
