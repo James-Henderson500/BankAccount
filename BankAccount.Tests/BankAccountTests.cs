0246bd122053e34d2177c4d1e4958ce08e879947
Rowan <Rowan.lowe@digital.homeoffice.gov.uk>
2025-11-12 13:14:34 +0000

    Add unit tests for BankAccount functionality and update project references

---
+++ BankAccount.Tests/BankAccountTests.cs
@@ -0,0 +1,95 @@
+﻿using Xunit;
+
+namespace BankAccountSystem.Tests;
+using BankAccountSystem;
+public class BankAccountTests
+{
+
+    [Fact]
+    public void CanCreateBankAccount()
+    {
+        // Arrange & Act
+        var account = new BankAccount();
+
+        // Assert
+        Assert.NotNull(account);
+    }
+
+    [Fact]
+    public void InterestOnlyAppliesToPostiveBalances()
+    {
+        var account = new BankAccount();
+        account.Deposit(1000);
+        decimal interest = account.CalculateInterest();
+        Assert.Equal(30, interest);
+
+        var emptyAccount = new BankAccount();
+        decimal noInterest = emptyAccount.CalculateInterest();
+        Assert.Equal(0, noInterest);
+
+    }
+
+    [Fact]
+    public void InterestRateIsThreePercentByDefault()
+    {
+        // Arrange
+        var account = new BankAccount();
+
+        // Act & Assert
+        Assert.Equal(0.03, account.InterestRate);
+    }
+
+    [Fact]
+    public void CanChangeInterestRate()
+    {
+        // Arrange
+        var account = new BankAccount();
+
+        // Act
+        account.SetInterestRate(0.10);
+
+        // Assert
+        Assert.Equal(0.10, account.InterestRate);
+    }
+
+    [Fact]
+    public void InterestPaidAnnually()
+    {
+        // Arrange
+        var account = new BankAccount();
+
+        account.SetInterestRate(0.10);
+        account.Deposit(10000);
+        int years_to_pay = 1;
+
+        // Act
+
+        account.PayInterest(years_to_pay);
+
+        // Assert
+
+        Assert.Equal(account.GetBalance(), 11000);
+    }
+
+    [Fact]
+    public void AnnualInterestCompoundsAnnually()
+    {
+        // Arrange
+        var account = new BankAccount();
+        var expected_balance = 12100;
+        account.SetInterestRate(0.10);
+        account.Deposit(10000);
+        int years_to_pay = 2;
+
+        // Act
+
+        account.PayInterest(years_to_pay);
+
+        // Assert
+
+        Assert.Equal(account.GetBalance(), expected_balance);
+    }
+    
+
+
+}
