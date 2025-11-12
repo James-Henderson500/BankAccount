a62dd1d4387b83c2712e5ef2ff6562aa947035f1
James-Henderson500 <james.henderson4@homeoffice.gov.uk>
2025-11-12 12:21:17 +0000

    Resolved merge

--- BankAccount.cs
+++ BankAccount.cs
@@ -17,10 +17,11 @@ namespace BankAccountSystem
         public decimal CalculateInterest()
         {
             if (balance > 0)
-            return balance * InterestRate;
+            {
+                return balance * InterestRate;
+            }
+            return 0;
         }
-        return 0;
-    }
     public decimal CalculateCompoundInterest(int years)
     {
         if (balance > 0 && years > 0)
@@ -40,27 +41,6 @@ namespace BankAccountSystem
             Console.WriteLine("Deposit amount must be positive.");
         }
     }
-    public void Withdraw(decimal amount)
-    {
-        if (amount > 0)
-        {
-            if (amount <= balance)
-            {
-                return balance * InterestRate;
-            }
-            return 0;
-        }
-        public void Deposit(decimal amount)
-        {
-            if (amount > 0)
-            {
-                balance += amount;
-            }
-            else
-            {
-                Console.WriteLine("Deposit amount must be positive.");
-            }
-        }
         public void Withdraw(decimal amount)
         {
             if (amount > 0)
